d78699acda7bb0c40725544aa9a515b5fe263706
Rasto Levrinc <rasto@linbit.com>
2009-08-26 17:54:24 +0200

    fixes for debian lenny

--- src/drbd/configs/DistResource_debian_LENNY.java
+++ src/drbd/configs/DistResource_debian_LENNY.java
@@ -46,32 +46,32 @@ public class DistResource_debian_LENNY extends
         // TODO: use flags for this
         {"Support", "debian-LENNY"},
 
-        /* openais/pacemaker opensuse */
-        {"PmInst.install.text.1",
-         "http://download.opensuse.org repository"},
+        ///* openais/pacemaker opensuse */
+        //{"PmInst.install.text.1",
+        // "http://download.opensuse.org repository"},
 
-        {"PmInst.install.1",
-         "echo 'deb http://download.opensuse.org/repositories/server:/ha-clustering/Debian_5.0/ ./'"
-         + " > /etc/apt/sources.list.d/ha-clustering.list "
-         + " && apt-get update"
-         + " && apt-get -y -q  --allow-unauthenticated install"
-         + " -o 'DPkg::Options::force=--force-confnew' pacemaker"
-         + " && (/usr/sbin/update-rc.d corosync start 75 2 3 4 5 . stop 05 0 1 6 . "
-         + "     || /usr/sbin/update-rc.d openais start 75 2 3 4 5 . stop 05 0 1 6 .) "
-         + " && if [ -e /etc/ais/openais.conf ];then"
-         + " mv /etc/ais/openais.conf /etc/ais/openais.conf.orig; fi;"
-         + " if [ -e /etc/corosync/corosync.conf ]; then"
-         + " mv /etc/corosync/corosync.conf /etc/corosync/corosync.conf.orig; fi"},
+        //{"PmInst.install.1",
+        // "echo 'deb http://download.opensuse.org/repositories/server:/ha-clustering/Debian_5.0/ ./'"
+        // + " > /etc/apt/sources.list.d/ha-clustering.list "
+        // + " && apt-get update"
+        // + " && apt-get -y -q  --allow-unauthenticated install"
+        // + " -o 'DPkg::Options::force=--force-confnew' pacemaker"
+        // + " && (/usr/sbin/update-rc.d corosync start 75 2 3 4 5 . stop 05 0 1 6 . "
+        // + "     || /usr/sbin/update-rc.d openais start 75 2 3 4 5 . stop 05 0 1 6 .) "
+        // + " && if [ -e /etc/ais/openais.conf ];then"
+        // + " mv /etc/ais/openais.conf /etc/ais/openais.conf.orig; fi;"
+        // + " if [ -e /etc/corosync/corosync.conf ]; then"
+        // + " mv /etc/corosync/corosync.conf /etc/corosync/corosync.conf.orig; fi"},
 
-        {"PmInst.install.files.1",
-         "openais-debian:/etc/init.d/openais:0755:"
-         + "openais-default-debian:/etc/default/openais:0644"},
+        //{"PmInst.install.files.1",
+        // "openais-debian:/etc/init.d/openais:0755:"
+        // + "openais-default-debian:/etc/default/openais:0644"},
 
         /* openais/pacemaker madkiss */
-        {"PmInst.install.text.2",
+        {"PmInst.install.text.1",
          "LINBIT/MADKISS repository (testing)"},
 
-        {"PmInst.install.2",
+        {"PmInst.install.1",
          "echo 'deb http://people.debian.org/~madkiss/ha lenny main'"
          + " > /etc/apt/sources.list.d/ha-clustering.list "
          + " && apt-get update"
@@ -82,31 +82,34 @@ public class DistResource_debian_LENNY extends
          + " && if [ -e /etc/corosync/corosync.conf ]; then"
          + " mv /etc/corosync/corosync.conf /etc/corosync/corosync.conf.orig; fi"},
 
-        /* heartbeat/pacemaker opensuse */
-        {"HbPmInst.install.text.1", "http://download.opensuse.org repository"},
+        ///* heartbeat/pacemaker opensuse */
+        //{"HbPmInst.install.text.1", "http://download.opensuse.org repository"},
 
-        {"HbPmInst.install.1",
-         "echo 'deb http://download.opensuse.org/repositories/server:/ha-clustering/Debian_5.0/ ./' > /etc/apt/sources.list.d/ha-clustering.list "
-         + " && apt-get update"
-         + " && apt-get -y -q  --allow-unauthenticated install"
-         + " -o 'DPkg::Options::force=--force-confnew' heartbeat pacemaker"
-         + " && /usr/sbin/update-rc.d heartbeat start 75 2 3 4 5 . stop 05 0 1 6 . "},
+        //{"HbPmInst.install.1",
+        // "echo 'deb http://download.opensuse.org/repositories/server:/ha-clustering/Debian_5.0/ ./' > /etc/apt/sources.list.d/ha-clustering.list "
+        // + " && apt-get update"
+        // + " && apt-get -y -q  --allow-unauthenticated install"
+        // + " -o 'DPkg::Options::force=--force-confnew' heartbeat pacemaker"
+        // + " && /usr/sbin/update-rc.d heartbeat start 75 2 3 4 5 . stop 05 0 1 6 . "},
 
         /* heartbeat/pacemaker madkiss */
-        {"HbPmInst.install.text.2",
+        {"HbPmInst.install.text.1",
          "LINBIT/MADKISS repository (testing: pacemaker-heartbeat)"},
 
-        {"HbPmInst.install.2",
+        {"HbPmInst.install.1",
          "echo 'deb http://people.debian.org/~madkiss/ha lenny main' > /etc/apt/sources.list.d/ha-clustering.list "
          + " && apt-get update"
          + " && apt-get -y -q  --allow-unauthenticated install"
-         + " -o 'DPkg::Options::force=--force-confnew' pacemaker-heartbeat"},
+         + " -o 'DPkg::Options::force=--force-confnew' pacemaker-heartbeat"
+         + " && chmod g+w /var/run/heartbeat/crm" // TODO: remove workarounds
+                                                  // when not needed
+         + " && ln -s /var/run/heartbeat/crm/ /var/run/crm"},
 
         /* heartbeat apt-get install */
-        {"HbPmInst.install.text.3",
+        {"HbPmInst.install.text.2",
          "lenny repository (not recommended)"},
 
-        {"HbPmInst.install.3",
+        {"HbPmInst.install.2",
          "apt-get update && "
          + "/usr/bin/apt-get -y -q install -o"
          + " 'DPkg::Options::force=--force-confnew' heartbeat-2"},
