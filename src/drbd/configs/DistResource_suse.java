c03f40d4d8984edca438f4bfa7d5e3d6f555697c
Rasto Levrinc <rasto@linbit.com>
2010-07-23 17:29:05 +0200

    add support file for opensuse 11.3

--- src/drbd/configs/DistResource_suse.java
+++ src/drbd/configs/DistResource_suse.java
@@ -43,6 +43,7 @@ public class DistResource_suse extends
         {"version:SUSE Linux Enterprise Server 10*", "SLES10"}, // detected
         {"version:openSUSE 11.1*",    "OPENSUSE11_1"}, // detected
         {"version:openSUSE 11.2*",    "OPENSUSE11_2"}, // detected
+        {"version:openSUSE 11.3*",    "OPENSUSE11_3"}, // detected
         {"version:SUSE Linux Enterprise Server 11*", "SLES11"}, // detected
         {"version:sles9",  "SLES9"}, // chosen
         {"version:sles10", "SLES10"}, // chosen
