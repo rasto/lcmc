2b0c429135fad5b7ba34482e1298b5cee211babc
Rasto Levrinc <rasto@linbit.com>
2011-06-28 21:20:47 +0200

    don't open dialog in swing thread, it makes gnome wnck-applet to go crazy

--- src/java/drbd/gui/dialog/ConfigDialog.java
+++ src/java/drbd/gui/dialog/ConfigDialog.java
@@ -455,50 +455,46 @@ public abstract class ConfigDialog {
             /* create option pane */
             final JPanel b = body();
             final MyButton dbc = defaultButtonClass;
-            Tools.invokeAndWait(new Runnable() {
-                @Override public void run() {
-                    optionPane = new JOptionPane(
-                                        b,
-                                        getMessageType(),
-                                        JOptionPane.DEFAULT_OPTION,
-                                        icon(),
-                                        allOptions.toArray(
-                                         new JComponent[allOptions.size()]),
-                                        dbc);
-                    optionPane.setPreferredSize(
-                                        new Dimension(dialogWidth(),
-                                                      dialogHeight()));
-                    optionPane.setMaximumSize(
-                                        new Dimension(dialogWidth(),
-                                                      dialogHeight()));
-                    optionPane.setMinimumSize(
-                                        new Dimension(dialogWidth(),
-                                                      dialogHeight()));
+            optionPane = new JOptionPane(
+                                b,
+                                getMessageType(),
+                                JOptionPane.DEFAULT_OPTION,
+                                icon(),
+                                allOptions.toArray(
+                                 new JComponent[allOptions.size()]),
+                                dbc);
+            optionPane.setPreferredSize(
+                                new Dimension(dialogWidth(),
+                                              dialogHeight()));
+            optionPane.setMaximumSize(
+                                new Dimension(dialogWidth(),
+                                              dialogHeight()));
+            optionPane.setMinimumSize(
+                                new Dimension(dialogWidth(),
+                                              dialogHeight()));
 
-                    optionPane.setBackground(
-                               Tools.getDefaultColor(
-                                          "ConfigDialog.Background.Dark"));
-                    final Container mainFrame =
-                                        Tools.getGUIData().getMainFrame();
-                        if (mainFrame instanceof JApplet) {
-                            dialogPanel =
-                               optionPane.createDialog((JApplet) mainFrame,
-                                                       getDialogTitle());
-                        } else {
-                            dialogPanel = optionPane.createDialog(
-                                                        (JFrame) mainFrame,
-                                                        getDialogTitle());
-                        }
-                    dialogPanel.setModal(false);
-                    dialogPanel.setResizable(true);
-                    dialogPanel.setPreferredSize(
-                            new Dimension(dialogWidth(), dialogHeight()));
-                    dialogPanel.setMaximumSize(
-                            new Dimension(dialogWidth(), dialogHeight()));
-                    dialogPanel.setMinimumSize(
-                            new Dimension(dialogWidth(), dialogHeight()));
+            optionPane.setBackground(
+                       Tools.getDefaultColor(
+                                  "ConfigDialog.Background.Dark"));
+            final Container mainFrame =
+                                Tools.getGUIData().getMainFrame();
+                if (mainFrame instanceof JApplet) {
+                    dialogPanel =
+                       optionPane.createDialog((JApplet) mainFrame,
+                                               getDialogTitle());
+                } else {
+                    dialogPanel = optionPane.createDialog(
+                                                (JFrame) mainFrame,
+                                                getDialogTitle());
                 }
-            });
+            dialogPanel.setModal(false);
+            dialogPanel.setResizable(true);
+            dialogPanel.setPreferredSize(
+                    new Dimension(dialogWidth(), dialogHeight()));
+            dialogPanel.setMaximumSize(
+                    new Dimension(dialogWidth(), dialogHeight()));
+            dialogPanel.setMinimumSize(
+                    new Dimension(dialogWidth(), dialogHeight()));
             /* set location like the previous dialog */
         }
         /* add action listeners */
--- src/java/drbd/utilities/RoboTest.java
+++ src/java/drbd/utilities/RoboTest.java
@@ -2053,7 +2053,7 @@ public final class RoboTest {
     private static void startTestE(final Robot robot, final Cluster cluster) {
         slowFactor = 0.2f;
         aborted = false;
-        int count = 50;
+        int count = 100;
         for (int i = count; i > 0; i--) {
             Tools.info("1 I: " + i);
             moveTo(robot, 300 , 200); /* host */
