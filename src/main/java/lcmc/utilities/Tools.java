rasto/lcmc

commit c5e9fe58e487b6b624ad0e96418ff188f409bca0

improve sorting of clusters and block devices

src/main/java/lcmc/utilities/Tools.java
@@ -2903,4 +2903,47 @@ public final class Tools {
         }
         return mac.toString();
     }
+
+    private static List<String> getNameParts(final String name) {
+        final List<String> parts = new ArrayList<String>();
+        if (name == null) {
+            return parts;
+        }
+        final Pattern p = Pattern.compile("(\\d+|\\D+)");
+        final Matcher m = p.matcher(name);
+        while (m.find()) {
+            parts.add(m.group());
+        }
+        return parts;
+    }
+
+    /**
+     * Compare two names, doing the right thing there are numbers in the
+     * beginning or in the end of the string.
+     */
+    public static int compareNames(final String s1, final String s2) {
+        final List<String> parts1 = getNameParts(s1);
+        final List<String> parts2 = getNameParts(s2);
+        int i = 0;
+        for (final String p1 : parts1) {
+            if (i >= parts2.size()) {
+                return 1;
+            }
+            final String p2 = parts2.get(i);
+            int res;
+            if (Character.isDigit(p1.charAt(0))) {
+                res = Long.compare(Long.parseLong(p1), Long.parseLong(p2));
+            } else {
+                res = p1.compareToIgnoreCase(p2);
+            }
+            if (res != 0) {
+                return res;
+            }
+            i++;
+        }
+        if (parts1.size() == parts2.size()) {
+            return 0;
+        }
+        return -1;
+    }
 }

src/main/java/lcmc/data/Cluster.java
@@ -47,7 +47,7 @@ import java.awt.Window;
  * @version $Id$
  *
  */
-public final class Cluster {
+public final class Cluster implements Comparable<Cluster> {
     /** Serial version UID. */
     private static final long serialVersionUID = 1L;
     /** Name of the cluster. */
@@ -381,5 +381,11 @@ public final class Cluster {
             }
         }
         return null;
+    }
+
+    /** Compares ignoring case. */
+    @Override
+    public int compareTo(final Cluster c) {
+        return Tools.compareNames(getName(), c.getName());
     }
 }

src/main/java/lcmc/data/Clusters.java
@@ -23,6 +23,7 @@
 package lcmc.data;
 
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.LinkedHashSet;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -42,7 +43,7 @@ public final class Clusters {
     /** Serial version UID. */
     private static final long serialVersionUID = 1L;
     /** Set of cluster objects. */
-    private final Set<Cluster> clusters = new LinkedHashSet<Cluster>();
+    private final Set<Cluster> clusters = new TreeSet<Cluster>();
     /** Clusters set lock. */
     private final ReadWriteLock mClustersLock = new ReentrantReadWriteLock();
     /** Clusters set read lock. */
@@ -80,16 +81,12 @@ public final class Clusters {
 
     /** Gets set of clusters. */
     public Set<Cluster> getClusterSet() {
-        final Set<Cluster> copy = new LinkedHashSet<Cluster>();
         mClustersReadLock.lock();
         try {
-            for (final Cluster c : clusters) {
-                copy.add(c);
-            }
+            return new LinkedHashSet<Cluster>(clusters);
         } finally {
             mClustersReadLock.unlock();
         }
-        return copy;
     }
 
     /** Return default name with incremented index. */

src/main/java/lcmc/gui/resources/AllHostsInfo.java
@@ -219,7 +219,7 @@ public final class AllHostsInfo extends Info {
         mainPanel.setBackground(Color.WHITE);
 
         final Set<Cluster> clusters =
-                Tools.getConfigData().getClusters().getClusterSet();
+                           Tools.getConfigData().getClusters().getClusterSet();
         if (clusters != null) {
             final JPanel bPanel =
                            new JPanel(new BorderLayout());

src/main/java/lcmc/gui/resources/Info.java
@@ -789,7 +789,7 @@ public class Info implements Comparable<Info> {
     /** Compares ignoring case. */
     @Override
     public int compareTo(final Info o) {
-        return toString().compareToIgnoreCase(o.toString());
+        return Tools.compareNames(toString(), o.toString());
     }
 
     /** Retruns panel with table and border. */

src/test/java/lcmc/utilities/ToolsTest1.java
@@ -20,6 +20,7 @@ import javax.swing.JCheckBox;
 import java.net.InetAddress;
 import lcmc.Exceptions;
 import lcmc.data.Host;
+import mockit.Deencapsulation;
 
 public final class ToolsTest1 extends TestCase {
     @Before
@@ -1447,5 +1448,47 @@ public final class ToolsTest1 extends TestCase {
     public void testGenerateVMMacAddress() {
        final String mac = Tools.generateVMMacAddress();
        assertEquals(mac.length(), 17);
+    }
+
+    public String invokeGetNameParts(Object name) {
+        if (name == null) {
+            name = String.class;
+        }
+        return Tools.join(",", (List<String>) Deencapsulation.invoke(
+                                                                Tools.class,
+                                                                "getNameParts",
+                                                                name));
+    }
+    @Test
+    public void testGetNameParts() {
+        assertEquals("22, aa ,11", invokeGetNameParts("22 aa 11"));
+        assertEquals("22", invokeGetNameParts("22"));
+        assertEquals("aa", invokeGetNameParts("aa"));
+        assertEquals("Cluster ,1", invokeGetNameParts("Cluster 1"));
+        assertEquals("", invokeGetNameParts(""));
+        assertEquals("", invokeGetNameParts(null));
+    }
+
+    @Test
+    public void testCompareNames() {
+        assertTrue(Tools.compareNames("a", "a") == 0);
+        assertTrue(Tools.compareNames("a", "b") < 0);
+        assertTrue(Tools.compareNames("10a", "2a") > 0);
+        assertTrue(Tools.compareNames("1a", "2a") < 0);
+        assertTrue(Tools.compareNames("2a", "2a") == 0);
+        assertTrue(Tools.compareNames("2a1", "2a") > 0);
+        assertTrue(Tools.compareNames("2a", "2a1") < 0);
+        assertTrue(Tools.compareNames("a10", "a2") > 0);
+        assertTrue(Tools.compareNames("a10b", "a2b") > 0);
+        assertTrue(Tools.compareNames("a2b", "a10b") < 0);
+        assertTrue(Tools.compareNames("a2b3", "a10b") < 0);
+        assertTrue(Tools.compareNames("a2b", "a10b3") < 0);
+        assertTrue(Tools.compareNames("1a2b3c4", "1a2b3c4") == 0);
+        assertTrue(Tools.compareNames(null, null) == 0);
+        assertTrue(Tools.compareNames("a", "") > 0);
+        assertTrue(Tools.compareNames("", "a") < 0);
+        assertTrue(Tools.compareNames("1", "") > 0);
+        assertTrue(Tools.compareNames("1", null) > 0);
+        assertTrue(Tools.compareNames(null, "1") < 0);
     }
 }

commit b14e4817341e8776bc041ba5eeab3a236f52701e

gui: fix problem with java 6 when compiled with java 7
introduced in c5e9fe58e487b6b624ad0e96418ff188f409bca0
"improve sorting of clusters and block devices"

src/main/java/lcmc/utilities/Tools.java
@@ -2920,7 +2920,7 @@ public final class Tools {
             final String p2 = parts2.get(i);
             int res;
             if (Character.isDigit(p1.charAt(0))) {
-                res = Long.compare(Long.parseLong(p1), Long.parseLong(p2));
+                res = Integer.parseInt(p1) - Integer.parseInt(p2);
             } else {
                 res = p1.compareToIgnoreCase(p2);
             }