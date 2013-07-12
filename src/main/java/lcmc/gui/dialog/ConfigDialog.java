ca01da337f81743032df43a40e2339ec1a95ab78
Rasto Levrinc <rasto.levrinc@gmail.com>
2013-07-12 22:01:34 +0200

    fix cleanup after closing of dialogs with close button

--- src/main/java/lcmc/gui/dialog/ConfigDialog.java
+++ src/main/java/lcmc/gui/dialog/ConfigDialog.java
@@ -59,6 +59,8 @@ import javax.swing.event.DocumentEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.concurrent.CountDownLatch;
 
 import java.beans.PropertyChangeEvent;
@@ -509,6 +511,12 @@ public abstract class ConfigDialog {
                         dialogPanel = new JDialog((JFrame) mainFrame);
                         dialogPanel.setContentPane(optionPane);
                     }
+                    dialogPanel.addWindowListener(new WindowAdapter() {
+                        @Override
+                        public void windowClosing(WindowEvent e) {
+                            disposeDialog();
+                        }
+                    });
                     dialogPanel.setModal(false);
                     dialogPanel.setResizable(true);
                 }
