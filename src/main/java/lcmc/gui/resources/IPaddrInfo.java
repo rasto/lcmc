962320e3d09110a00b3f15d6db357360520c3558
Rasto Levrinc <rasto.levrinc@gmail.com>
2014-04-14 21:41:19 +0200

    fix exception switching to advanced mode java.lang.NullPointerException null lcmc.gui.resources.IPaddrInfo.checkResourceFields(IPaddrInfo.java:80) lcmc.gui.ClusterBrowser.checkAccessOfEverything(ClusterBrowser.java:2589) lcmc.utilities.Tools.checkAccessOfEverything(Tools.java:2488) lcmc.gui.MainMenu$13$1.run(MainMenu.java:658)

--- src/main/java/lcmc/gui/resources/IPaddrInfo.java
+++ src/main/java/lcmc/gui/resources/IPaddrInfo.java
@@ -77,6 +77,9 @@ final class IPaddrInfo extends ServiceInfo {
         } else {
             return check;
         }
+        if (wi == null) {
+            return check;
+        }
         wi.setEditable(true);
         wi.selectSubnet();
         final String ip = wi.getStringValue();
