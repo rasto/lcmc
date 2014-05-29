cfd6412f9260852611c3719307d27b3c81bf8df9
Rasto Levrinc <rasto.levrinc@gmail.com>
2014-05-29 19:35:24 +0200

    cleanup: extract PopupHostKeyVerifier class

--- src/main/java/lcmc/utilities/ssh/SSH.java
+++ src/main/java/lcmc/utilities/ssh/SSH.java
@@ -25,20 +25,16 @@ package lcmc.utilities.ssh;
 import ch.ethz.ssh2.ChannelCondition;
 import ch.ethz.ssh2.Connection;
 import ch.ethz.ssh2.InteractiveCallback;
-import ch.ethz.ssh2.KnownHosts;
 import ch.ethz.ssh2.LocalPortForwarder;
 import ch.ethz.ssh2.SCPClient;
-import ch.ethz.ssh2.ServerHostKeyVerifier;
 import ch.ethz.ssh2.Session;
 import ch.ethz.ssh2.channel.ChannelManager;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-
 import lcmc.configs.DistResource;
 import lcmc.data.Host;
 import lcmc.gui.ProgressBar;
@@ -1207,113 +1203,6 @@ public final class SSH {
         }
     }
 
-    /**
-     * This ServerHostKeyVerifier asks the user on how to proceed if a key
-     * cannot befound in the in-memory database.
-     */
-    private class AdvancedVerifier implements ServerHostKeyVerifier {
-        /** Verifies the keys. */
-        @Override
-        public boolean verifyServerHostKey(final String hostname,
-                                           final int port,
-                                           final String serverHostKeyAlgorithm,
-                                           final byte[] serverHostKey)
-        throws Exception {
-            final String hostString = hostname;
-            final String algo = serverHostKeyAlgorithm;
-            final StringBuilder message = new StringBuilder(200);
-
-            /* Check database */
-            final int result =
-                        Tools.getApplication().getKnownHosts().verifyHostkey(
-                                                      hostname,
-                                                      serverHostKeyAlgorithm,
-                                                      serverHostKey);
-
-            switch (result) {
-            case KnownHosts.HOSTKEY_IS_OK:
-                return true;
-
-            case KnownHosts.HOSTKEY_IS_NEW:
-                message.append("Do you want to accept the hostkey (type ");
-                message.append(algo);
-                message.append(") from ");
-                message.append(host);
-                message.append(" ?\n");
-                break;
-
-            case KnownHosts.HOSTKEY_HAS_CHANGED:
-                message.append("WARNING! Hostkey for ");
-                message.append(hostString);
-                message.append(" has changed!\nAccept anyway?\n");
-                break;
-
-            default:
-                throw new IllegalStateException();
-            }
-
-            /* Include the fingerprints in the message */
-            final String hexFingerprint =
-                        KnownHosts.createHexFingerprint(serverHostKeyAlgorithm,
-                                                        serverHostKey);
-            final String bubblebabbleFingerprint =
-                                    KnownHosts.createBubblebabbleFingerprint(
-                                                      serverHostKeyAlgorithm,
-                                                      serverHostKey);
-
-            message.append("Hex Fingerprint: ");
-            message.append(hexFingerprint);
-            message.append("\nBubblebabble Fingerprint: ");
-            message.append(bubblebabbleFingerprint);
-
-            /* Now ask the user */
-            final int choice =
-                            sshGui.getConfirmDialogChoice(message.toString());
-            if (sshGui.isConfirmDialogYes(choice)) {
-
-                /* Be really paranoid. We use a hashed hostname entry */
-                final String hashedHostname =
-                                     KnownHosts.createHashedHostname(hostname);
-
-                /* Add the hostkey to the in-memory database */
-                Tools.getApplication().getKnownHosts().addHostkey(
-                                                new String[] {hashedHostname},
-                                                serverHostKeyAlgorithm,
-                                                serverHostKey);
-
-                /* Also try to add the key to a known_host file */
-                /* It dows this only in Linux.
-                 * TODO: do this also for other OSes, when I find out the
-                 * known_hosts locations.
-                 */
-                if (Tools.isWindows()) {
-                    LOG.debug("verifyServerHostKey: not using known_hosts"
-                              + " file, because this is Windows.");
-                } else {
-                    try {
-                        KnownHosts.addHostkeyToFile(
-                              new File(
-                                    Tools.getApplication().getKnownHostPath()),
-                              new String[] {hashedHostname},
-                              serverHostKeyAlgorithm,
-                              serverHostKey);
-                    } catch (final IOException ignore) {
-                        LOG.appWarning("verifyServerHostKey: SSH "
-                                       + "addHostKeyToFile failed "
-                                       + ignore.getMessage());
-                    }
-                }
-
-                return true;
-            }
-
-            if (sshGui.isConfirmDialogCancel(choice)) {
-                throw new Exception(
-                          "The user aborted the server hostkey verification.");
-            }
-            return false;
-        }
-    }
 
     /**
      * The logic that one has to implement if "keyboard-interactive"
@@ -1473,7 +1362,7 @@ public final class SSH {
                                     }
 
                                     conn.close();
-                                    conn.connect(new AdvancedVerifier(),
+                                    conn.connect(new PopupHostKeyVerifier(sshGui),
                                                  connectTimeout,
                                                  kexTimeout);
                                 }
@@ -1498,7 +1387,7 @@ public final class SSH {
                                     }
 
                                     conn.close();
-                                    conn.connect(new AdvancedVerifier(),
+                                    conn.connect(new PopupHostKeyVerifier(sshGui),
                                                  connectTimeout,
                                                  kexTimeout);
                                 }
@@ -1530,7 +1419,7 @@ public final class SSH {
                                     break;
                                 }
                                 conn.close();
-                                conn.connect(new AdvancedVerifier(),
+                                conn.connect(new PopupHostKeyVerifier(sshGui),
                                              connectTimeout,
                                              kexTimeout);
                             }
@@ -1553,7 +1442,7 @@ public final class SSH {
                                     break;
                                 }
                                 conn.close();
-                                conn.connect(new AdvancedVerifier(),
+                                conn.connect(new PopupHostKeyVerifier(sshGui),
                                              connectTimeout,
                                              kexTimeout);
                             }
@@ -1766,7 +1655,7 @@ public final class SSH {
                 //    }
                 //};
                 //conn.addConnectionMonitor(connectionMonitor);
-                conn.connect(new AdvancedVerifier(),
+                conn.connect(new PopupHostKeyVerifier(sshGui),
                              connectTimeout,
                              kexTimeout);
 
