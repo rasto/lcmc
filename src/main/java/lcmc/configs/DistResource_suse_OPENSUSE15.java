951ca16c9c8737723cbf573522c5505a39db7964
Rasto Levrinc <rasto.levrinc@gmail.com>
2019-06-23 23:03:30 +0200

    Fix opensuse corosync/pacemaker starting/stopping

---
+++ src/main/java/lcmc/configs/DistResource_suse_OPENSUSE15.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of DRBD Management Console by LINBIT HA-Solutions GmbH
+ * written by Rasto Levrinc.
+ *
+ * Copyright (C) 2009, LINBIT HA-Solutions GmbH.
+ * Copyright (C) 2011-2012, Rastislav Levrinc.
+ *
+ * DRBD Management Console is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * DRBD Management Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with drbd; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package lcmc.configs;
+
+import java.util.Arrays;
+import java.util.ListResourceBundle;
+
+/**
+ * Here are commands for opensuse 13.1.
+ */
+public final class DistResource_suse_OPENSUSE15 extends ListResourceBundle {
+
+    private static final Object[][] contents = {
+
+        {"distributiondir", "sles11"},
+        {"Support", "suse-OPENSUSE13_1"},
+
+        {"Corosync.startCorosync",
+         DistResource.SUDO + "systemctl start pacemaker.service"},
+
+        {"Corosync.startPcmk",
+         DistResource.SUDO + "systemctl start pacemaker.service"},
+
+        {"Corosync.stopCorosync",
+         DistResource.SUDO + "systemctl stop pacemaker.service"},
+
+        {"Corosync.stopCorosyncWithPcmk",
+         DistResource.SUDO + "systemctl stop pacemaker.service"},
+
+        {"Corosync.startCorosyncWithPcmk",
+         DistResource.SUDO + "systemctl start pacemaker.service;"},
+
+        {"Corosync.reloadCorosync",
+         "if ! " + DistResource.SUDO + "systemctl status pacemaker.service >/dev/null 2>&1; then "
+         + DistResource.SUDO + "systemctl start pacemaker.service; fi"},
+
+        {"Corosync.addToRc",
+         DistResource.SUDO + "systemctl enable pacemaker.service"},
+
+    };
+
+    @Override
+    protected Object[][] getContents() {
+        return Arrays.copyOf(contents, contents.length);
+    }
+}
