a23873b80ee21fc51ebc0946ad85898b2bf862cc
Rasto Levrinc <rasto.levrinc@gmail.com>
2020-03-23 22:16:06 +0100

    Tidy-up

--- src/main/java/lcmc/vm/domain/VMCreator.java
+++ src/main/java/lcmc/vm/domain/VMCreator.java
@@ -20,22 +20,23 @@
 
 package lcmc.vm.domain;
 
-import lcmc.common.domain.XMLTools;
-import lcmc.logger.Logger;
-import lcmc.logger.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.inject.Named;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import javax.inject.Named;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import lcmc.common.domain.XMLTools;
+import lcmc.logger.Logger;
+import lcmc.logger.LoggerFactory;
 
 @Named
 class VMCreator {
@@ -59,174 +60,73 @@ class VMCreator {
                                 final String domainName,
                                 final boolean needConsole,
                                 final String type) {
-        final Element root = (Element) doc.appendChild(doc.createElement("domain"));
+        final var root = (Element) doc.appendChild(doc.createElement("domain"));
         root.setAttribute("type", type); /* kvm/xen */
-        final Node uuidNode = root.appendChild(doc.createElement("uuid"));
-        uuidNode.appendChild(doc.createTextNode(uuid));
-        final Node nameNode = root.appendChild(doc.createElement("name"));
-        nameNode.appendChild(doc.createTextNode(domainName));
-        final Node memoryNode = root.appendChild(doc.createElement("memory"));
-        final long mem = Long.parseLong(parametersMap.get(VMParams.VM_PARAM_MEMORY));
+        createTextNode(root, "uuid", uuid);
+        createTextNode(root, "name", domainName);
+        final var memoryNode = root.appendChild(doc.createElement("memory"));
+        final var mem = Long.parseLong(parametersMap.get(VMParams.VM_PARAM_MEMORY));
         memoryNode.appendChild(doc.createTextNode(Long.toString(mem)));
-        final Node curMemoryNode = root.appendChild(doc.createElement("currentMemory"));
-        final long curMem = Long.parseLong(parametersMap.get(VMParams.VM_PARAM_CURRENTMEMORY));
+        final var curMemoryNode = root.appendChild(doc.createElement("currentMemory"));
+        final var curMem = Long.parseLong(parametersMap.get(VMParams.VM_PARAM_CURRENTMEMORY));
         curMemoryNode.appendChild(doc.createTextNode(Long.toString(curMem)));
-        final String vcpu = parametersMap.get(VMParams.VM_PARAM_VCPU);
-        if (vcpu != null) {
-            final Node vcpuNode = root.appendChild(doc.createElement("vcpu"));
-            vcpuNode.appendChild(doc.createTextNode(vcpu));
-        }
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_VCPU))
+                .ifPresent(vcpu -> createTextNode(root, "vcpu", vcpu));
 
-        final String bootloader = parametersMap.get(VMParams.VM_PARAM_BOOTLOADER);
-        if (bootloader != null) {
-            final Node bootloaderNode = root.appendChild(doc.createElement("bootloader"));
-            bootloaderNode.appendChild(doc.createTextNode(bootloader));
-        }
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_BOOTLOADER))
+                .ifPresent(bootloader -> createTextNode(root, "bootloader", bootloader));
 
-        final Node osNode = root.appendChild(doc.createElement("os"));
-        final Element typeNode = (Element) osNode.appendChild(doc.createElement("type"));
+        final var osNode = root.appendChild(doc.createElement("os"));
+        final var typeNode = (Element) osNode.appendChild(doc.createElement("type"));
         typeNode.appendChild(doc.createTextNode(parametersMap.get(VMParams.VM_PARAM_TYPE)));
         typeNode.setAttribute("arch", parametersMap.get(VMParams.VM_PARAM_TYPE_ARCH));
         typeNode.setAttribute("machine", parametersMap.get(VMParams.VM_PARAM_TYPE_MACHINE));
-        final String init = parametersMap.get(VMParams.VM_PARAM_INIT);
-        if (init != null && !"".equals(init)) {
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_INIT))
+                .filter(it -> !it.isBlank())
+                .ifPresent(init -> {
             final Node initNode = osNode.appendChild(doc.createElement("init"));
             initNode.appendChild(doc.createTextNode(init));
-        }
-        final Element bootNode = (Element) osNode.appendChild(doc.createElement(VMParams.OS_BOOT_NODE));
+        });
+        final var bootNode = (Element) osNode.appendChild(doc.createElement(VMParams.OS_BOOT_NODE));
         bootNode.setAttribute(VMParams.OS_BOOT_NODE_DEV, parametersMap.get(VMParams.VM_PARAM_BOOT));
-        final String bootDev2 = parametersMap.get(VMParams.VM_PARAM_BOOT_2);
-        if (bootDev2 != null && !"".equals(bootDev2)) {
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_BOOT_2))
+                .filter(it -> !it.isBlank())
+                .ifPresent(bootDev2 -> {
             final Element bootNode2 = (Element) osNode.appendChild(doc.createElement(VMParams.OS_BOOT_NODE));
             bootNode2.setAttribute(VMParams.OS_BOOT_NODE_DEV, parametersMap.get(VMParams.VM_PARAM_BOOT_2));
-        }
+        });
 
         Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_LOADER))
-                .map(String::trim)
-                .filter(it -> !it.isEmpty())
+                .filter(it -> !it.isBlank())
                 .ifPresent(loader -> osNode.appendChild(doc.createElement("loader"))
                                            .appendChild(doc.createTextNode(loader)));
         addFeatures(root);
         addClockOffset(root);
         addCPUMatchNode(root);
 
-        final String onPoweroff = parametersMap.get(VMParams.VM_PARAM_ON_POWEROFF);
-        if (onPoweroff != null) {
-            final Node onPoweroffNode = root.appendChild(doc.createElement("on_poweroff"));
-            onPoweroffNode.appendChild(doc.createTextNode(onPoweroff));
-        }
-        final String onReboot = parametersMap.get(VMParams.VM_PARAM_ON_REBOOT);
-        if (onReboot != null) {
-            final Node onRebootNode = root.appendChild(doc.createElement("on_reboot"));
-            onRebootNode.appendChild(doc.createTextNode(onReboot));
-        }
-        final String onCrash = parametersMap.get(VMParams.VM_PARAM_ON_CRASH);
-        if (onCrash != null) {
-            final Node onCrashNode = root.appendChild(doc.createElement("on_crash"));
-            onCrashNode.appendChild(doc.createTextNode(onCrash));
-        }
-        final String emulator = parametersMap.get(VMParams.VM_PARAM_EMULATOR);
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_ON_POWEROFF))
+                .ifPresent(onPoweroff -> createTextNode(root, "on_poweroff", onPoweroff));
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_ON_REBOOT))
+                .ifPresent(onReboot -> createTextNode(root, "on_reboot", onReboot));
+
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_ON_CRASH))
+                .ifPresent(onCrash -> createTextNode(root, "on_crash", onCrash));
+        final var emulator = parametersMap.get(VMParams.VM_PARAM_EMULATOR);
         if (emulator != null || needConsole) {
-            final Node devicesNode = root.appendChild(doc.createElement("devices"));
+            final var devicesNode = root.appendChild(doc.createElement("devices"));
             if (needConsole) {
-                final Element consoleNode = (Element) devicesNode.appendChild(doc.createElement("console"));
+                final var consoleNode = (Element) devicesNode.appendChild(doc.createElement("console"));
                 consoleNode.setAttribute("type", "pty");
             }
-            final Node emulatorNode = devicesNode.appendChild(doc.createElement("emulator"));
+            final var emulatorNode = devicesNode.appendChild(doc.createElement("emulator"));
             emulatorNode.appendChild(doc.createTextNode(emulator));
         }
         return root;
     }
 
-    public void addCPUMatchNode(final Node root) {
-        final String cpuMatch = parametersMap.get(VMParams.VM_PARAM_CPU_MATCH);
-        final Element cpuMatchNode = (Element) root.appendChild(doc.createElement("cpu"));
-        if (!"".equals(cpuMatch)) {
-            cpuMatchNode.setAttribute("match", cpuMatch);
-        }
-        final String model = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_MODEL);
-        if (!"".equals(model)) {
-            final Node modelNode = cpuMatchNode.appendChild(doc.createElement("model"));
-            modelNode.appendChild(doc.createTextNode(model));
-        }
-        final String vendor = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_VENDOR);
-        if (!"".equals(vendor)) {
-            final Node vendorNode = cpuMatchNode.appendChild(doc.createElement("vendor"));
-            vendorNode.appendChild(doc.createTextNode(vendor));
-        }
-        final String sockets = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_TOPOLOGY_SOCKETS);
-        final String cores = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_TOPOLOGY_CORES);
-        final String threads = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_TOPOLOGY_THREADS);
-        final boolean isSockets = !"".equals(sockets);
-        final boolean isCores = !"".equals(cores);
-        final boolean isThreads = !"".equals(threads);
-        if (isSockets || isCores || isThreads) {
-            final Element topologyNode = (Element) cpuMatchNode.appendChild(doc.createElement("topology"));
-            if (isSockets) {
-                topologyNode.setAttribute("sockets", sockets);
-            }
-            if (isCores) {
-                topologyNode.setAttribute("cores", cores);
-            }
-            if (isThreads) {
-                topologyNode.setAttribute("threads", threads);
-            }
-        }
-        final String policy = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_FEATURE_POLICY);
-        final String features = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_FEATURES);
-        if (policy != null && features != null && !"".equals(policy) && !"".equals(features)) {
-            for (final String feature : features.split("\\s+")) {
-                final Element featureNode = (Element) cpuMatchNode.appendChild(doc.createElement("feature"));
-                featureNode.setAttribute("policy", policy);
-                featureNode.setAttribute("name", feature);
-            }
-        }
-        if (!cpuMatchNode.hasChildNodes()) {
-            root.removeChild(cpuMatchNode);
-        }
-    }
-
-    public void addFeatures(final Node root) {
-        final boolean acpi = "True".equals(parametersMap.get(VMParams.VM_PARAM_ACPI));
-        final boolean apic = "True".equals(parametersMap.get(VMParams.VM_PARAM_APIC));
-        final boolean pae = "True".equals(parametersMap.get(VMParams.VM_PARAM_PAE));
-        final boolean hap = "True".equals(parametersMap.get(VMParams.VM_PARAM_HAP));
-        if (acpi || apic || pae || hap) {
-            final Node featuresNode = root.appendChild(doc.createElement("features"));
-            if (acpi) {
-                featuresNode.appendChild(doc.createElement("acpi"));
-            }
-            if (apic) {
-                featuresNode.appendChild(doc.createElement("apic"));
-            }
-            if (pae) {
-                featuresNode.appendChild(doc.createElement("pae"));
-            }
-            if (hap) {
-                featuresNode.appendChild(doc.createElement("hap"));
-            }
-        }
-    }
-
-    public void addClockOffset(final Node root) {
-        final Element clockNode = (Element) root.appendChild(doc.createElement("clock"));
-        final String offset = parametersMap.get(VMParams.VM_PARAM_CLOCK_OFFSET);
-        clockNode.setAttribute("offset", offset);
-        final Element timer1 = (Element) clockNode.appendChild(doc.createElement("timer"));
-        timer1.setAttribute("name", "pit");
-        timer1.setAttribute("tickpolicy", "delay");
-        final Element timer2 = (Element) clockNode.appendChild(doc.createElement("timer"));
-        timer2.setAttribute("name", "rtc");
-        timer2.setAttribute("tickpolicy", "catchup");
-
-        final Element timer3 = (Element) clockNode.appendChild(doc.createElement("timer"));
-        timer3.setAttribute("name", "hpet");
-        timer3.setAttribute("present", "no");
-    }
-
     public void modifyDomain(final Node domainNode) {
-        final XPath xpath = XPathFactory.newInstance().newXPath();
-        final Map<String, String> paths = new HashMap<String, String>();
+        final var xpath = XPathFactory.newInstance().newXPath();
+        final Map<String, String> paths = new HashMap<>();
         paths.put(VMParams.VM_PARAM_MEMORY, "memory");
         paths.put(VMParams.VM_PARAM_CURRENTMEMORY, "currentMemory");
         paths.put(VMParams.VM_PARAM_VCPU, "vcpu");
@@ -286,12 +186,6 @@ class VMCreator {
                     node.setAttribute("arch", value);
                 } else if (VMParams.VM_PARAM_TYPE_MACHINE.equals(param)) {
                     node.setAttribute("machine", value);
-                } else if (VMParams.VM_PARAM_CPU_MATCH.equals(param)) {
-                    if ("".equals(value)) {
-                        node.getParentNode().removeChild(node);
-                    } else {
-                        node.setAttribute("match", value);
-                    }
                 } else if (VMParams.VM_PARAM_CPUMATCH_TOPOLOGY_THREADS.equals(param)) {
                     node.setAttribute("threads", value);
                 } else {
@@ -310,4 +204,91 @@ class VMCreator {
             LOG.appError("modifyDomainXML: could not evaluate: ", e);
         }
     }
+
+    private void createTextNode(Element root, String name, String text) {
+        final var uuidNode = root.appendChild(doc.createElement(name));
+        uuidNode.appendChild(doc.createTextNode(text));
+    }
+
+    private void addCPUMatchNode(final Node root) {
+        final var cpuMatchNode = (Element) root.appendChild(doc.createElement("cpu"));
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_CPU_MATCH))
+                .filter(it -> !it.isBlank())
+                .ifPresent(cpuMatch -> cpuMatchNode.setAttribute("match", cpuMatch));
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_CPUMATCH_MODEL))
+                .filter(it -> !it.isBlank())
+                .ifPresent(model -> createTextNode(cpuMatchNode, "model", model));
+        Optional.ofNullable(parametersMap.get(VMParams.VM_PARAM_CPUMATCH_VENDOR))
+                .filter(it -> !it.isBlank())
+                .ifPresent(vendor -> createTextNode(cpuMatchNode, "vendor", vendor));
+        final var sockets = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_TOPOLOGY_SOCKETS);
+        final var cores = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_TOPOLOGY_CORES);
+        final var threads = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_TOPOLOGY_THREADS);
+        final boolean isSockets = !"".equals(sockets);
+        final boolean isCores = !"".equals(cores);
+        final boolean isThreads = !"".equals(threads);
+        if (isSockets || isCores || isThreads) {
+            final Element topologyNode = (Element) cpuMatchNode.appendChild(doc.createElement("topology"));
+            if (isSockets) {
+                topologyNode.setAttribute("sockets", sockets);
+            }
+            if (isCores) {
+                topologyNode.setAttribute("cores", cores);
+            }
+            if (isThreads) {
+                topologyNode.setAttribute("threads", threads);
+            }
+        }
+        final var policy = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_FEATURE_POLICY);
+        final var features = parametersMap.get(VMParams.VM_PARAM_CPUMATCH_FEATURES);
+        if (policy != null && features != null && !"".equals(policy) && !"".equals(features)) {
+            for (final String feature : features.split("\\s+")) {
+                final Element featureNode = (Element) cpuMatchNode.appendChild(doc.createElement("feature"));
+                featureNode.setAttribute("policy", policy);
+                featureNode.setAttribute("name", feature);
+            }
+        }
+        if (!cpuMatchNode.hasChildNodes()) {
+            root.removeChild(cpuMatchNode);
+        }
+    }
+
+    private void addFeatures(final Node root) {
+        final boolean acpi = "True".equals(parametersMap.get(VMParams.VM_PARAM_ACPI));
+        final boolean apic = "True".equals(parametersMap.get(VMParams.VM_PARAM_APIC));
+        final boolean pae = "True".equals(parametersMap.get(VMParams.VM_PARAM_PAE));
+        final boolean hap = "True".equals(parametersMap.get(VMParams.VM_PARAM_HAP));
+        if (acpi || apic || pae || hap) {
+            final Node featuresNode = root.appendChild(doc.createElement("features"));
+            if (acpi) {
+                featuresNode.appendChild(doc.createElement("acpi"));
+            }
+            if (apic) {
+                featuresNode.appendChild(doc.createElement("apic"));
+            }
+            if (pae) {
+                featuresNode.appendChild(doc.createElement("pae"));
+            }
+            if (hap) {
+                featuresNode.appendChild(doc.createElement("hap"));
+            }
+        }
+    }
+
+    private void addClockOffset(final Node root) {
+        final var clockNode = (Element) root.appendChild(doc.createElement("clock"));
+        final var offset = parametersMap.get(VMParams.VM_PARAM_CLOCK_OFFSET);
+        clockNode.setAttribute("offset", offset);
+        final var timer1 = (Element) clockNode.appendChild(doc.createElement("timer"));
+        timer1.setAttribute("name", "pit");
+        timer1.setAttribute("tickpolicy", "delay");
+        final var timer2 = (Element) clockNode.appendChild(doc.createElement("timer"));
+        timer2.setAttribute("name", "rtc");
+        timer2.setAttribute("tickpolicy", "catchup");
+
+        final var timer3 = (Element) clockNode.appendChild(doc.createElement("timer"));
+        timer3.setAttribute("name", "hpet");
+        timer3.setAttribute("present", "no");
+    }
+
 }
