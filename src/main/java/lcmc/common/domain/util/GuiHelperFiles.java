97ddf799a7efd15e51ab671295e963c9f488b724
Rasto Levrinc <rasto.levrinc@gmail.com>
2020-04-06 08:18:46 +0200

    Fix helper file loading on more hosts

--- src/main/java/lcmc/common/domain/util/GuiHelperFiles.java
+++ src/main/java/lcmc/common/domain/util/GuiHelperFiles.java
@@ -1,27 +1,55 @@
 package lcmc.common.domain.util;
 
+import java.net.URI;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import lombok.SneakyThrows;
+import lombok.Synchronized;
 import lombok.val;
 
 public class GuiHelperFiles {
     private static final String GUI_HELPER_DIR = "/help-progs/lcmc-gui-helper/";
     private static final String GUI_HELPER_FILENAME = GUI_HELPER_DIR + "Main.pl";
+    private final URI dirUri;
+
+    @SneakyThrows
+    public GuiHelperFiles() {
+        dirUri = Tools.class.getResource(GUI_HELPER_DIR).toURI();
+    }
 
     public String readGuiHelper() {
         return Tools.readFile(GUI_HELPER_FILENAME) + inlinePerlModules();
     }
 
-    @SneakyThrows
     private String inlinePerlModules() {
-        Path start = dirPath();
-        try (val paths = Files.walk(start, 1)) {
+        if (isJarScheme()) {
+            return withFilesystem(this::readPerlModules);
+        } else {
+            return withFiles(this::readPerlModules);
+        }
+    }
+
+    @SneakyThrows
+    @Synchronized
+    private String withFilesystem(Function<Path, String> function) {
+        try (val fileSystem = FileSystems.newFileSystem(dirUri, Collections.emptyMap())) {
+            return function.apply(fileSystem.getPath(GUI_HELPER_DIR));
+        }
+    }
+
+    private String withFiles(Function<Path, String> function) {
+        return function.apply(Paths.get(dirUri));
+    }
+
+    @SneakyThrows
+    private String readPerlModules(Path dirPath) {
+        try (val paths = Files.walk(dirPath, 1)) {
             return paths.filter(path -> path.toString().endsWith(".pm"))
                     .map(this::readPerlModule)
                     .map(this::surroundWithParenthesis)
@@ -29,23 +57,13 @@ public class GuiHelperFiles {
         }
     }
 
-    @SneakyThrows
-    private Path dirPath() {
-        val uri = Tools.class.getResource(GUI_HELPER_DIR).toURI();
-        if (uri.getScheme().equals("jar")) {
-            val fileSystem = FileSystems.newFileSystem(uri, Collections.emptyMap());
-            return fileSystem.getPath(GUI_HELPER_DIR);
-        } else {
-            return Paths.get(uri);
-        }
+    private boolean isJarScheme() {
+        return "jar".equals(dirUri.getScheme());
     }
 
     @SneakyThrows
     private String readPerlModule(Path path) {
-        return Files.readAllLines(path)
-                    .stream()
-                    .map(line -> "    " + line)
-                    .collect(Collectors.joining("\n"));
+        return Files.readAllLines(path).stream().map(line -> "    " + line).collect(Collectors.joining("\n"));
 
     }
 
