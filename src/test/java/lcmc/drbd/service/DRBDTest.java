9d94b19f2ecebb658857a63fefcf9b987f429790
Rasto Levrinc <rasto.levrinc@gmail.com>
2021-04-05 20:25:05 +0200

    Switch to junit 5 WIP

--- src/test/java/lcmc/drbd/service/DRBDTest.java
+++ src/test/java/lcmc/drbd/service/DRBDTest.java
@@ -1,37 +1,33 @@
 package lcmc.drbd.service;
 
-import static junitparams.JUnitParamsRunner.$;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import junitparams.Parameters;
-
 @ExtendWith(MockitoExtension.class)
-final class DRBDTest {
-    private Object[] parametersForVersionsShouldBeCompatible() {
-        return $($("8.3.11", "8.3.11"), $("8.4.1", "8.4.9"), $("8.3.11", "8.3.13"), $("8.4.3.4", "8.4.1"), $("8.4.3", "8.4.1.4"),
-                $("9.0.0", "9.0.5.4"));
-    }
-
-    @Test
-    @Parameters(method = "parametersForVersionsShouldBeCompatible")
+class DRBDTest {
+    @ParameterizedTest
+    @CsvSource({"8.3.11, 8.3.11, 8.4.1, 8.4.9, 8.3.11 8.3.13, 8.4.3.4, 8.4.1, 8.4.3, 8.4.1.4, 9.0.0, 9.0.5.4"})
     void versionsShouldBeCompatible(final String versionOne, final String versionTwo) {
         assertTrue(DRBD.compatibleVersions(versionOne, versionTwo));
         assertTrue(DRBD.compatibleVersions(versionTwo, versionOne));
     }
 
-    private Object[] parametersForVersionsShouldNotBeCompatible() {
-        return $( 
-            $("8.4.2", "9.0.5"), $("8.3.13", "8.4.3"), $("8.3.11", "8.4.3"), $("8.3.11", "8.4.3"), $(null, "8.4.1"), $(null, null),
-                $("1", "8.4.2"));
+    private static Stream<Arguments> parametersForVersionsShouldNotBeCompatible() {
+        return Stream.of(Arguments.of("8.4.2", "9.0.5"), Arguments.of("8.3.13", "8.4.3"), Arguments.of("8.3.11", "8.4.3"),
+                Arguments.of("8.3.11", "8.4.3"), Arguments.of(null, "8.4.1"), Arguments.of(null, null), Arguments.of("1", "8.4.2"));
     }
 
-    @Test
-    @Parameters(method = "parametersForVersionsShouldNotBeCompatible")
+    @ParameterizedTest
+    @MethodSource("parametersForVersionsShouldNotBeCompatible")
     void versionsShouldNotBeCompatible(final String versionOne, final String versionTwo) {
         assertFalse(versionTwo + " not compatible with " + versionOne, DRBD.compatibleVersions(versionOne, versionTwo));
         assertFalse(versionOne + " not compatible with " + versionTwo, DRBD.compatibleVersions(versionTwo, versionOne));
